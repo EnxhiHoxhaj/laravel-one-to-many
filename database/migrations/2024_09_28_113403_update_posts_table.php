19b13e83440ba00cbcdc3cbc72245c1018de4d16
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-09-28 15:06:35 +0200

    feat add foreign key to post

--- database/migrations/2024_09_28_113403_update_posts_table.php
+++ database/migrations/2024_09_28_113403_update_posts_table.php
@@ -24,7 +24,7 @@ return new class extends Migration
     {
         Schema::table('posts', function (Blueprint $table) {
             $table->dropForeign(['category_id']);
-        $table->dropColumn();
+            $table->dropColumn('category_id');
         });
     }
 };
